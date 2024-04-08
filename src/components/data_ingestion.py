ee65c300999c56f8a83e28a1d990321b4e3109c2
arivasyam <arivasyamm.com>
2024-04-08 19:15:33 +0700

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,8 +3,9 @@ import sys
 
 import pandas as pd
 
-from src.exception import CustomExecptiom
+from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -42,9 +43,12 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomExecptiom(e,sys)
+            raise CustomException(e,sys)
             
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
